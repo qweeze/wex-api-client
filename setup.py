6585e9cb9ad76d927d84bff061be592c5e6ceb1c
George Fortunatov <qweeeze@gmail.com>
2017-09-17 03:37:16 +0500

    update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     name='wex',
     author='qweeze',
     author_email='qweeeze@gmail.com',
-    version = '0.1',
-    description='https://wex.nz exchange API client',
+    version='0.1',
+    description='wex.nz exchange API client',
+    long_description='A python client for https://wex.nz cryptocurrency exchange',
     url='https://github.com/qweeze/wex-api-client',
     packages=find_packages('.')
 )
