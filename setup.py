449c15b15ee60aef70e31f02118e9026ffd5b0c3
Kevin Deldycke <kevin@deldycke.com>
2017-09-17 03:21:30 +0500

    add setup

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='wex',
     author='qweeze',
     author_email='qweeeze@gmail.com',
+    version = '0.1',
     description='https://wex.nz exchange API client',
+    url='https://github.com/qweeze/wex-api-client',
     packages=find_packages('.')
 )
